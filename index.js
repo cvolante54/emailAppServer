98c54b66772ec2e5193dec62d3c2c76d541df5d2
Carlos <cvolantesilva@gmail.com>
2017-12-26 19:45:20 -1000

    ready for Heroku deployment

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+	res.send({ hi: "there" });
+});
+
+//Server set-up
+const PORT = process.env.PORT || 4000;
+// const server = http.createServer(app);
+
+app.listen(PORT, function(err) {
+	console.log("Server running in port: " + port);
+});
