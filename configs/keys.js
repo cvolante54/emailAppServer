67b68ad616240d564e1a0a379eaf1ec8e2665bc2
Carlos <cvolantesilva@gmail.com>
2017-12-28 21:35:21 -1000

    authentication basics ready

---
+++ configs/keys.js
@@ -0,0 +1,6 @@
+if (process.env.NODE_ENV === 'production') {
+	//we are in production
+} else {
+	//we are in development
+	module.exports = require('./dev');
+}
---
+++ configs/prod.js
@@ -0,0 +1,9 @@
+module.exports = {
+	googleClientID: process.env.GOOGLE_CLIENT_ID,
+	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+	mongoURI: process.env.MONGO_URI,
+	facebookAppID: process.env.FACEBOOK_APP_ID,
+	facebookAppSecret: process.env.FACEBOOK_APP_SECRET,
+	cookieKey: process.env.COOKIE_KEY
+};
+
